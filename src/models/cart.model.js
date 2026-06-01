5de48d450ce42ca07e0d11a9412d16c5339debc7
siva <sivavarma1999@gmail.com>
2026-06-01 13:39:55 +0530

    create cart model with user and product references

---
+++ src/models/cart.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+        products:[{
+            productId:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "product",
+                required: true,
+            }
+        }],
+    },
+    { timestamps: true },
+);
+
+const cartModel = mongoose.model("cart", cartSchema);
+
+export default cartModel;
